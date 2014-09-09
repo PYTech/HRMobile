c71e5159244a96958b34461dd86046cdb5ba07f9
pytarng <pytarng@gmail.com>
2014-09-10 01:26:44 +0800

    1. add http and https rest client 2. remove unused libs

--- src/com/pytech/hrm/activities/MainActivity.java
+++ src/com/pytech/hrm/activities/MainActivity.java
@@ -1,5 +1,19 @@
 package com.pytech.hrm.activities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +21,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.pytech.hrm.R;
+import com.pytech.hrm.util.constants.REST;
+import com.pytech.hrm.util.rest.HRMHttpClientFactory;
 
 public class MainActivity extends Activity {
 
@@ -14,7 +30,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		if(true) {
 			startActivity(new Intent(this, LoginActivity.class));
 		}
@@ -34,8 +50,27 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	public void general() {
-		
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				DefaultHttpClient httpClient = HRMHttpClientFactory.createClient();
+				
+				HttpPost request = new HttpPost(REST.SERVER_URL + REST.LOGIN_PATH);
+				List<NameValuePair> form = new ArrayList<NameValuePair>();
+				form.add(new BasicNameValuePair(REST.KEY_FORM_USERNAME, "benny"));
+				form.add(new BasicNameValuePair(REST.KEY_FORM_MIMA, "123456"));
+
+				try {
+					request.setEntity(new UrlEncodedFormEntity(form, HTTP.UTF_8));
+					HttpResponse response = httpClient.execute(request);
+					List<Cookie> cookies = httpClient.getCookieStore().getCookies();
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
 	}
 }
